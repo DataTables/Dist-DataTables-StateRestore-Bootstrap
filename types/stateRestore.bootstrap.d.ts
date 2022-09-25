5f91689a7f66d83630b1ea62baff22a7fab78290
Allan Jardine <allan@datatables.net>
2022-09-25 15:50:15 +0000

    Sync DistRepos

--- types/stateRestore.bootstrap.d.ts
+++ types/stateRestore.bootstrap.d.ts
@@ -1,7 +1,4 @@
 /*! Bootstrap integration for DataTables' StateRestore
- * ©2016 SpryMedia Ltd - datatables.net/license
+ * © SpryMedia Ltd - datatables.net/license
  */
-declare let define: {
-    (stringValue: any, Function: any): any;
-    amd: string;
-};
+declare var DataTable: any;
