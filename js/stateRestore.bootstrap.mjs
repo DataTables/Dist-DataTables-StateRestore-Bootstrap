307a55330c37bed12fe07b70d3e116ebdf196bab
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:49 +0000

    Add support for mjs files (ES Modules)

---
+++ js/stateRestore.bootstrap.mjs
@@ -0,0 +1,24 @@
+
+/*! Bootstrap integration for DataTables' StateRestore
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import DataTable from '';
+
+
+$.extend(true, DataTable.StateRestoreCollection.classes, {
+    checkBox: 'dtsr-check-box form-check-input',
+    creationButton: 'dtsr-creation-button btn btn-default',
+    creationForm: 'dtsr-creation-form modal-body',
+    creationText: 'dtsr-creation-text modal-header',
+    creationTitle: 'dtsr-creation-title modal-title',
+    nameInput: 'dtsr-name-input form-control'
+});
+$.extend(true, DataTable.StateRestore.classes, {
+    confirmationButton: 'dtsr-confirmation-button btn btn-default',
+    confirmationTitle: 'dtsr-confirmation title modal-header',
+    input: 'dtsr-input form-control'
+});
+
+
+export default DataTable;
