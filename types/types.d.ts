3dfb3828c3348316da27c0670918e2674e8fd3b3
Allan Jardine <allan@datatables.net>
2022-10-27 16:04:49 +0000

    Correct Nuget jQuery UI dependency + Fix multiple exports from styling files

--- types/types.d.ts
+++ types/types.d.ts
@@ -5,38 +5,3 @@ import Api from "datatables.net";
 
 export default Api;
 
-
-// Dist-DataTables-StateRestore-Bootstrap integration with Bootstrap exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bootstrap integration with Bootstrap exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bootstrap integration with Bootstrap exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bootstrap integration with Bootstrap exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bootstrap integration with Bootstrap exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
