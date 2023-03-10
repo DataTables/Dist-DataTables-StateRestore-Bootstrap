43e0d290702bd942e286c13361f6407174cf642b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-03-10 17:21:52 +0000

    f5d08bd36e235d9596c94c72127e3c8ef58ba119 1.2.2 release
    Sync to source repo @f5d08bd36e235d9596c94c72127e3c8ef58ba119

--- js/stateRestore.bootstrap.mjs
+++ js/stateRestore.bootstrap.mjs
@@ -6,7 +6,6 @@ import $ from 'jquery';
 import DataTable from 'datatables.net-bs';
 import StateRestore from 'datatables.net-staterestore';
 
-
 $.extend(true, DataTable.StateRestoreCollection.classes, {
     checkBox: 'dtsr-check-box form-check-input',
     creationButton: 'dtsr-creation-button btn btn-default',
